b19c4461de6c88206459031df85d2e69e04666e6
Juan G. Victores <jgvictores@users.noreply.github.com>
2017-05-29 12:20:30 +0200

    Update correct-typical-errors.sh

--- scripts/correct-typical-errors.sh
+++ scripts/correct-typical-errors.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+# Punctuation
+grep "\.\." *.tex | grep -v "\.\.\." | grep -v "\.\.\/"  # sed: \.
+
+# 
 grep "This way" *.tex  # sed: omit
 grep "But" *.tex  # sed: omit or replace by however
-grep "\.\." *.tex | grep -v "\.\.\." | grep -v "\.\.\/"  # sed: \.
+
+# 
+grep -i "problem" *.tex # sed: issue, challenge
